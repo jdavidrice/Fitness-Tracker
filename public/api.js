20ae81d0dc90c0d13967b8ba7e9115851c024849
Jeremy Rice <jdavidrice@gmail.com>
2021-03-04 12:39:29 -0500

    Make eslint changes in js files

--- public/api.js
+++ public/api.js
@@ -1,44 +1,89 @@
+/* eslint-disable no-unused-vars */
 const API = {
-  async getLastWorkout() {
-    let res;
-    try {
-      res = await fetch("/api/workouts");
-    } catch (err) {
-      console.log(err)
-    }
-    const json = await res.json();
 
-    return json[json.length - 1];
-  },
-  async addExercise(data) {
-    const id = location.search.split("=")[1];
+	async getLastWorkout() {
 
-    const res = await fetch("/api/workouts/" + id, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
-    });
+		let res;
 
-    const json = await res.json();
+		try {
 
-    return json;
-  },
-  async createWorkout(data = {}) {
-    const res = await fetch("/api/workouts", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: { "Content-Type": "application/json" }
-    });
+			res = await fetch("/api/workouts");
 
-    const json = await res.json();
+		} catch (err) {
 
-    return json;
-  },
+			console.log(err);
 
-  async getWorkoutsInRange() {
-    const res = await fetch(`/api/workouts/range`);
-    const json = await res.json();
+		}
 
-    return json;
-  },
+		const json = await res.json();
+
+
+
+		return json[json.length - 1];
+
+	},
+
+	async addExercise(data) {
+
+		const id = location.search.split("=")[1];
+
+
+
+		const res = await fetch("/api/workouts/" + id, {
+
+			method: "PUT",
+
+			headers: { "Content-Type": "application/json" },
+
+			body: JSON.stringify(data)
+
+		});
+
+
+
+		const json = await res.json();
+
+
+
+		return json;
+
+	},
+
+	async createWorkout(data = {}) {
+
+		const res = await fetch("/api/workouts", {
+
+			method: "POST",
+
+			body: JSON.stringify(data),
+
+			headers: { "Content-Type": "application/json" }
+
+		});
+
+
+
+		const json = await res.json();
+
+
+
+		return json;
+
+	},
+
+
+
+	async getWorkoutsInRange() {
+
+		const res = await fetch("/api/workouts/range");
+
+		const json = await res.json();
+
+
+
+		return json;
+
+	},
+
 };
+
