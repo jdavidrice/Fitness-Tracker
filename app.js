c8732c6afce1e2929dac4d5d917c6a2167667252
Jeremy Rice <jdavidrice@gmail.com>
2021-03-05 01:09:58 -0500

    Add logger and db info to app.js and remove blank lines from js files

--- app.js
+++ app.js
@@ -1,55 +1,41 @@
 require("dotenv").config();
-
 const express = require("express");
-
+const mongojs = require("mongojs");
 const helmet = require("helmet");
-
 const path = require("path");
-
-
+const logger = require("morgan");
 
 const app = express();
 
+app.use(logger("dev"));
+
 app.use(helmet());
-
-
-
-app.use(express.urlencoded({
-
-	extended: false
-
-}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "public")));
 
+const databasuUrl = "fitness";
+const collections = ["fitnessdata"];
 
+const db = mongojs(databasuUrl, collections);
+
+db.on("error", error => {
+  console.log("Database error:", error)
+})
 
 app.get("/", (_req, res) => {
-
-	return res.send(`<h1>Welcome to
-
-    <a href="https://github.com/jdavidrice/nodejs-repo-template" target="_blank">NodeJS-Repo-Template</a<</h1>`);
-
+  res.sendFile(path.join(__dirname + "./public/index.html"));
 });
 
-
+// Additional routes go here
 
 const PORT = process.env.PORT || 3000;
-
 const IP = process.env.IP || "127.0.0.1";
-
 app.listen(PORT, IP, (err) => {
-
-	if (err) {
-
-		console.log(err);
-
-	} else {
-
-		console.log(`Server is listening at: http://${IP}:${PORT}`);
-
-	}
-
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server is listening at: http://${IP}:${PORT}`);
+  }
 });
