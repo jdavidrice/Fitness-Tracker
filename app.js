a8013e59a445dce76f20975efdae16460b830480
Jeremy Rice <jdavidrice@gmail.com>
2021-03-05 14:03:17 -0500

    Add models folder

--- app.js
+++ app.js
@@ -1,36 +1,33 @@
 require("dotenv").config();
 const express = require("express");
-const mongojs = require("mongojs");
-const helmet = require("helmet");
+const mongoose = require("mongoose");
+// const helmet = require("helmet");
 const path = require("path");
 const logger = require("morgan");
 
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+// const User = require("./userModel.js");
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(helmet());
-app.use(express.urlencoded({ extended: false }));
-
+// app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
-const databasuUrl = "fitness";
-const collections = ["fitnessdata"];
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true });
 
-const db = mongojs(databasuUrl, collections);
-
-db.on("error", error => {
-    console.log("Database error:", error);
-});
 
 app.get("/", (_req, res) => {
     res.sendFile(path.join(__dirname + "./public/index.html"));
 });
 
-// Additional routes go here
 
-const PORT = process.env.PORT || 3000;
 const IP = process.env.IP || "127.0.0.1";
 app.listen(PORT, IP, (err) => {
     if (err) {
